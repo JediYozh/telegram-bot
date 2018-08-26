f1da9cda8d8b4582af7489e811c1078ef61678f1
JediYozh <kalach2010@gmail.com>
2018-08-26 20:27:42 +0300

    Add lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   linters: {
     '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
-      'eslint . --fix',
       'prettier --write',
       'git add',
     ],
